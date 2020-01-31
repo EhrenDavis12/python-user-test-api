7688e65697233dee07a86abfbab7b3ad48f9b361
Ehren Davis <ehren.davis12@gmail.com>
2020-01-30 22:21:02 -0700

    apply white list

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -104,7 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'https://user-activity-monitor.herokuapp.com',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
